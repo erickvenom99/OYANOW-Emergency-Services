b5b4d6ca0f6022f17ca2066559b61078fefe1c1a
ericvenom99 <prevenakhtar@mail.com>
2024-08-28 17:38:33 +0200

    updated the navbar.tsx and the home.tsx

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -7,17 +7,10 @@ import Banner from "../ReusableComponents/Banner/Banner";
 import Footer from "../ReusableComponents/Footer/Footer";
 
 const Home = () => {
-  const navItems = [
-    { id: "1", title: "Home", url: "/" },
-    { id: "2", title: "Electrical", url: "/features" },
-    { id: "3", title: "Mechanical", url: "/pricing" },
-    { id: "4", title: "Plumbering", url: "/testimonial" },
-    { id: "5", title: "Pricing", url: "/pricing" },
-    { id: "6", title: "Sign-in", url: "/sign-up" },
-  ];
+  
   return (
     <>
-      <Navbar navItems={navItems} />
+      <Navbar />
       <Banner />
       <Services />
       <AboutUs />
--- frontend/src/components/ReusableComponents/Navbar/Navbar.tsx
+++ frontend/src/components/ReusableComponents/Navbar/Navbar.tsx
@@ -4,57 +4,54 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import demoLogo from "../../../assets/demo-logo.png";
 import "./Navbar.css";
 
-interface Prop {
-  navItems: {
-    id: string;
-    title: string;
-    url: string;
-  }[];
-}
-
-const Navbar: React.FC<Prop> = ({ navItems }) => {
+const Navbar = () => {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  
   return (
-    <>
-      <section id="nav-bar">
-        <nav className="navbar navbar-expand-lg">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              <img src={demoLogo} alt="Demo Logo" className="navbar-logo" />
-            </a>
-            <button
-              className="navbar-toggler custom-toggle"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded={!isNavCollapsed}
-              aria-label="Toggle navigation"
-              onClick={handleNavCollapse}
-            >
-              <FontAwesomeIcon icon={faBars} />
-            </button>
-            <div
-              className={`collapse navbar-collapse${
-                isNavCollapsed ? "" : " show"
-              }`}
-              id="navbarNav"
-            >
-              <ul className="navbar-nav ms-auto">
-                {navItems.map((item) => (
-                  <li key={item.id} className="nav-item">
-                    <a className="nav-link" href={item.url}>
-                      {item.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+    <section id="nav-bar">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={demoLogo} alt="Demo Logo" className="navbar-logo" />
+          </a>
+          <button
+            className="navbar-toggler custom-toggle"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <div className={`collapse navbar-collapse${isNavCollapsed ? "" : " show"}`} id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/mechanical">Mechanical</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/electrical">Electrical</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/plumbering">Plumbering</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/signin">Sign-in</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">Contacts</a>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </section>
-    </>
+        </div>
+      </nav>
+    </section>
   );
 };
 
